1082242fea9425dc31c18d3ff800db300fb8de5c
Keran Braich <keran.braich@ft.com>
2019-02-01 14:52:41 +0000

    Implement yargs for options
    Currently, options have to be passed into the CLI tool in a particular
    order. The options, based on how they were passed in, are assigned to
    a variable and used throughout the script. By using yargs, we can avoid
    all of that complexity and users can pass in the option with a flag in
    whatever order they please.

---
+++ src/commands/run-script.js
@@ -0,0 +1,141 @@
+#!/usr/bin/env node
+
+const assert = require('assert');
+const fs = require('fs');
+
+const { Git } = require('@financial-times/tooling-helpers');
+const runProcess = require('../lib/run-process');
+
+/**
+ * yargs builder function.
+ *
+ * @param {import('yargs/yargs').Yargs} yargs - Instance of yargs
+ */
+const builder = (yargs) => {
+
+    return yargs
+        .option('workspace', {
+            alias: 'w',
+            describe: 'Path to a workspace directory',
+            demandOption: true,
+            type: 'string'
+        })
+        .option('script', {
+            alias: 's',
+            describe: 'Path to a script',
+            demandOption: true,
+            type: 'string'
+        })
+        .option('targets', {
+            describe: 'Target repositories - if multiple, must be comma separated',
+            demandOption: true,
+            type: 'string'
+        })
+        .option('branch', {
+            alias: 'b',
+            describe: 'Name for the git branch to create',
+            demandOption: true,
+            type: 'string'
+        })
+        .option('token', {
+            describe: 'GitHub Personal Access Token (must have all repo scopes)',
+            demandOption: true,
+            type: 'string'
+        })
+};
+
+/**
+ * yargs handler function.
+ *
+ * @param {object} argv - argv parsed and filtered by yargs
+ * @param {string} argv.workspace
+ * @param {string} argv.script
+ * @param {string} argv.targets
+ * @param {string} argv.branch
+ * @param {string} argv.token
+ */
+const handler = async ({ workspace, script, targets, branch, token }) => {
+
+    const workspacePathExists = fs.existsSync(workspace);
+    assert(workspacePathExists, `Workspace directory path does not exist: ${workspace}`);
+    const workspacePathIsDirectory = fs.lstatSync(workspace).isDirectory();
+    assert(workspacePathIsDirectory, `Workspace directory path is not a directory: ${workspace}`);
+
+    const scriptExists = fs.existsSync(script);
+    assert(scriptExists, `Script does not exist: ${script}`);
+    try {
+        fs.accessSync(script, fs.constants.X_OK);
+    } catch (err) {
+        assert(false, `Script is not executable (try \`chmod +x\`): ${script}`);
+    }
+
+    if (!targets) {
+        throw new Error('No targets specified')
+    }
+
+    console.log(`-- Workspace directory: ${workspace}`);
+    console.log(`-- Script: ${script}`);
+    console.log(`-- Target(s):\n\n   ${targets.split(',').join('\n   ')}`);
+
+    for (let repository of targets) {
+        console.log('\n===\n');
+
+        const repositoryName = repository.split('/').pop();
+        const cloneDirectory = `${workspace}/${repositoryName}`;
+
+        const git = new Git({
+            credentials: {
+                type: Git.CREDENTIAL_TYPE_GITHUB_OAUTH,
+                token
+            }
+        });
+
+        try {
+            console.log(`-- Cloning repository locally: ${repository}`);
+            const gitRepo = await git.clone({ repository, directory: cloneDirectory });
+            console.log(`-- Repository '${repositoryName}' cloned locally to ${cloneDirectory}`);
+
+            await gitRepo.createBranchAndCheckout({ branch });
+            console.log(`-- Created and checked out new branch in local repository: ${branch}`);
+
+            const contextForScript = {
+                TRANSFORMATION_RUNNER_RUNNING: true,
+                TRANSFORMATION_RUNNER_TARGET: repository,
+                TRANSFORMATION_RUNNER_TARGET_NAME: repositoryName,
+            };
+
+            const scriptEnv = {
+                ...process.env,
+                ...contextForScript
+            };
+
+            console.log(`-- Running script against local repository...\n`);
+
+            const scriptOutput  = await runProcess(
+                scriptPath,
+                {
+                    cwd: gitRepo.workingDirectory,
+                    env: scriptEnv
+                }
+            );
+
+            console.log(scriptOutput);
+
+            console.log(`-- Pushing branch ${branch} to remote 'origin'`);
+            await gitRepo.pushCurrentBranchToRemote();
+
+        } catch (error) {
+            console.error(new Error(`Error running script for '${repository}': ${error.message}`));
+        }
+    }
+};
+
+/**
+ * @see https://github.com/yargs/yargs/blob/master/docs/advanced.md#providing-a-command-module
+ */
+module.exports = {
+	command: 'run-script',
+	desc: 'Run a script against repositories',
+	builder,
+	handler,
+};
