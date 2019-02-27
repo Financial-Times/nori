bad5af1629e64367a78621970a54b721f98b6c0e
bren <bren@153.io>
2019-02-27 14:34:29 +0000

    boneless interactive prompt with some sketch stuff
    - does a loop and asks you for some stuff
    - maintains a list of steps to persist to disk and rehydrate
    - maintains an in memory object of things with types to pass around to other things

---
+++ src/commands/interactive.js
@@ -0,0 +1,103 @@
+#!/usr/bin/env node
+
+const {prompt} = require('enquirer');
+
+/**
+ * yargs builder function.
+ *
+ * @param {import('yargs/yargs').Yargs} yargs - Instance of yargs
+ */
+const builder = () => {};
+
+const operations = [
+    {
+        name: 'tako',
+        input: 'start',
+        output: 'repos',
+        prompt: () => prompt({}),
+        get: () => {}
+    },
+    {
+        name: 'ebi',
+        input: 'repos',
+        output: 'repos',
+        prompt: () => prompt({
+            name: 'query',
+            type: 'text'
+        }),
+        get: () => {},
+    },
+    {
+        name: 'run-script',
+        input: 'repos',
+        output: 'branches',
+        prompt: () => prompt({}),
+        get: () => {},
+    },
+    {
+        name: 'prs',
+        input: 'branches',
+        output: 'prs',
+        prompt: () => prompt({}),
+        get: () => {},
+    },
+    {
+        name: 'project',
+        input: 'prs',
+        output: false,
+        prompt: () => prompt({}),
+        get: () => {},
+    },
+    {
+        name: 'done',
+        input: false,
+        output: 'done',
+        prompt: () => prompt({}),
+        get: () => {},
+    }
+];
+
+/**
+ * yargs handler function.
+ *
+ * @param {object} argv - argv parsed and filtered by yargs
+ * @param {string} argv.workspace
+ * @param {string} argv.script
+ * @param {string} argv.targets
+ * @param {string} argv.branch
+ */
+const handler = async () => {
+    let type = 'start';
+    const steps = [];
+    const data = {};
+
+    while(type !== 'done') {
+        const choices = operations.filter(({input}) => !input || input === type);
+        const {thing} = await prompt({
+            name: 'thing',
+            message: 'what do',
+            type: 'select',
+            choices: choices.map(({name}) => ({name})),
+        });
+
+        const choice = choices.find(({name}) => name === thing);
+        type = choice.output;
+
+        const payload = await choice.prompt();
+        steps.push({type, payload});
+        
+        data[type] = await choice.get(payload, data);
+    }
+
+    console.log(steps, data);
+};
+
+/**
+ * @see https://github.com/yargs/yargs/blob/master/docs/advanced.md#providing-a-command-module
+ */
+module.exports = {
+	command: ['*', 'interactive'],
+	desc: 'Interactively build steps of a transformation',
+	builder,
+	handler,
+};
