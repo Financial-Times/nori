d872aa44e423f22eb00c4a516ed013175c2fc78a
bren <bren@153.io>
2019-03-18 16:18:04 +0000

    sort out indentation

--- src/commands/_interactive.js
+++ src/commands/_interactive.js
@@ -26,115 +26,115 @@ const workspacePath = path.join(process.env.HOME, '.config/transformation-runner
 */
 
 const operations = {
-    tako: require('./tako'),
-    file: require('./file'),
-    // require('./ebi'), // ebi isn't yet usable outside of the CLI
-    'silly-filter': {
-        command: 'silly-filter',
-        desc: 'silly filter test',
-        input: ['repos'],
-        output: 'repos',
-        args: [{
-            name: 'filter',
-            type: 'text'
-        }],
-        handler: ({filter, repos}) => repos.filter(repo => repo.name.includes(filter))
-    },
-    'run-script': require('./run-script'),
-    prs: require('./prs'),
-    project: require('./project'),
+	tako: require('./tako'),
+	file: require('./file'),
+	// require('./ebi'), // ebi isn't yet usable outside of the CLI
+	'silly-filter': {
+		command: 'silly-filter',
+		desc: 'silly filter test',
+		input: ['repos'],
+		output: 'repos',
+		args: [{
+			name: 'filter',
+			type: 'text'
+		}],
+		handler: ({filter, repos}) => repos.filter(repo => repo.name.includes(filter))
+	},
+	'run-script': require('./run-script'),
+	prs: require('./prs'),
+	project: require('./project'),
 };
 
 async function getResume() {
-    let steps = [];
-    let data = {};
-    let resume = 'new';
-    let run;
+	let steps = [];
+	let data = {};
+	let resume = 'new';
+	let run;
 
-    const previousRuns = (await fs.readdir(workspacePath)).filter(
-        file => file.endsWith('.json')
-    );
+	const previousRuns = (await fs.readdir(workspacePath)).filter(
+		file => file.endsWith('.json')
+	);
 
-    const mtimes = await Promise.all(
-        previousRuns.map(run =>
-            fs.stat(
-                path.join(workspacePath, run)
-            ).then(stat => stat.mtime)
-        )
-    );
+	const mtimes = await Promise.all(
+		previousRuns.map(run =>
+			fs.stat(
+				path.join(workspacePath, run)
+			).then(stat => stat.mtime)
+		)
+	);
 
-    const sortedRuns = previousRuns.map(
-        (run, index) => ({run, modified: mtimes[index]})
-    ).sort(
-        ({modified: a}, {modified: b}) => b - a
-    );
+	const sortedRuns = previousRuns.map(
+		(run, index) => ({run, modified: mtimes[index]})
+	).sort(
+		({modified: a}, {modified: b}) => b - a
+	);
 
-    if(previousRuns.length) {
-        ({resume} = await prompt({
-            name: 'resume',
-            type: 'select',
-            choices: sortedRuns.map(
-                ({run, modified}) => ({
-                    name: run,
-                    message: `${run} (${relativeDate(modified)})`,
-                })
-            ).concat(
-                {role: 'separator'},
-                {name: 'new'},
-                {name: 'edit'},
-            ),
-        }));
-    }
+	if(previousRuns.length) {
+		({resume} = await prompt({
+			name: 'resume',
+			type: 'select',
+			choices: sortedRuns.map(
+				({run, modified}) => ({
+					name: run,
+					message: `${run} (${relativeDate(modified)})`,
+				})
+			).concat(
+				{role: 'separator'},
+				{name: 'new'},
+				{name: 'edit'},
+			),
+		}));
+	}
 
-    if(resume === 'edit') {
-        const {toDelete, confirm} = await prompt([{
-            type: 'multiselect',
-            name: 'toDelete',
-            choices: sortedRuns.map(({run}) => run)
-        }, {
-            type: 'confirm',
-            name: 'confirm',
-            message: ({answers: {toDelete}}) => `delete ${toSentence(toDelete)}`,
-            skip() {
-                // should be first argument to skip, see https://github.com/enquirer/enquirer/issues/105
-                return this.state.answers.toDelete.length === 0;
-            }
-        }]);
+	if(resume === 'edit') {
+		const {toDelete, confirm} = await prompt([{
+			type: 'multiselect',
+			name: 'toDelete',
+			choices: sortedRuns.map(({run}) => run)
+		}, {
+			type: 'confirm',
+			name: 'confirm',
+			message: ({answers: {toDelete}}) => `delete ${toSentence(toDelete)}`,
+			skip() {
+				// should be first argument to skip, see https://github.com/enquirer/enquirer/issues/105
+				return this.state.answers.toDelete.length === 0;
+			}
+		}]);
 
-        if(confirm) {
-            await Promise.all(
-                toDelete.map(
-                    run => fs.unlink(
-                        path.join(workspacePath, run)
-                    )
-                )
-            );
-        }
+		if(confirm) {
+			await Promise.all(
+				toDelete.map(
+					run => fs.unlink(
+						path.join(workspacePath, run)
+					)
+				)
+			);
+		}
 
-        return getResume();
-    }
+		return getResume();
+	}
 
-    if(resume === 'new') {
-        const {name} = await prompt({
-            name: 'name',
-            type: 'text',
-        });
+	if(resume === 'new') {
+		const {name} = await prompt({
+			name: 'name',
+			type: 'text',
+		});
 
-        run = path.join(workspacePath, name + '.json');
-    } else {
-        run = path.join(workspacePath, resume);
-        ({steps, data} = JSON.parse(
-            await fs.readFile(run, 'utf8')
-        ));
-    }
+		run = path.join(workspacePath, name + '.json');
+	} else {
+		run = path.join(workspacePath, resume);
+		({steps, data} = JSON.parse(
+			await fs.readFile(run, 'utf8')
+		));
+	}
 
-    return {run, steps, data};
+	return {run, steps, data};
 }
 
 const takeWhileLast = (array, func) => (
-    array.length && func(array[array.length - 1])
-        ? takeWhileLast(array.slice(0, -1), func).concat(array.slice(-1))
-        : []
+	array.length && func(array[array.length - 1])
+		? takeWhileLast(array.slice(0, -1), func).concat(array.slice(-1))
+		: []
 )
 
 /**
@@ -147,111 +147,111 @@ const takeWhileLast = (array, func) => (
  * @param {string} argv.branch
  */
 const handler = async () => {
-    const {run, steps, data} = await getResume();
+	const {run, steps, data} = await getResume();
 
-    const persist = () => fs.writeFile(
-        run,
-        JSON.stringify({steps, data}, null, 2)
-    );
+	const persist = () => fs.writeFile(
+		run,
+		JSON.stringify({steps, data}, null, 2)
+	);
 
-    async function runStep({operation, args}) {
-        const dataArgs = operation.input.reduce(
-            (args, type) => Object.assign(args, {
-                [type]: data[type]
-            }),
-            {}
-        );
+	async function runStep({operation, args}) {
+		const dataArgs = operation.input.reduce(
+			(args, type) => Object.assign(args, {
+				[type]: data[type]
+			}),
+			{}
+		);
 
-        const stepData = await operation.handler(
-            Object.assign(dataArgs, args)
-        );
+		const stepData = await operation.handler(
+			Object.assign(dataArgs, args)
+		);
 
-        if(operation.output) {
-            steps.push({name: operation.command, args});
-            data[operation.output] = stepData;
-        }
+		if(operation.output) {
+			steps.push({name: operation.command, args});
+			data[operation.output] = stepData;
+		}
 
-        await persist();
-    }
+		await persist();
+	}
 
-    async function replay(steps) {
-        for(const step of steps) {
-            await runStep({
-                operation: operations[step.name],
-                args: step.args,
-            });
-        }
-    }
+	async function replay(steps) {
+		for(const step of steps) {
+			await runStep({
+				operation: operations[step.name],
+				args: step.args,
+			});
+		}
+	}
 
-    while(true) {
-        const header = Object.keys(data).map(
-            type => types[type].shortPreview(data[type])
-        ).filter(Boolean).join(' ∙ ');
+	while(true) {
+		const header = Object.keys(data).map(
+			type => types[type].shortPreview(data[type])
+		).filter(Boolean).join(' ∙ ');
 
-        const {thing} = await prompt({
-            name: 'thing',
-            message: 'what do',
-            type: 'select',
-            header,
-            choices: Object.values(operations).map(({command, desc, input, output}) => {
-                const dataHasInputs = input.every(type => type in data);
-                const dataHasOutput = output in data;
-                const isFilter = input.includes(output);
+		const {thing} = await prompt({
+			name: 'thing',
+			message: 'what do',
+			type: 'select',
+			header,
+			choices: Object.values(operations).map(({command, desc, input, output}) => {
+				const dataHasInputs = input.every(type => type in data);
+				const dataHasOutput = output in data;
+				const isFilter = input.includes(output);
 
-                // allow an operation if the data has all the inputs and the data doesn't
-                // have the output (i.e. this operation hasn't already been run) *unless*
-                // the operation has the same output as one of the inputs (i.e. it can be
-                // run multiple times on the same data)
-                const shouldAllowOperation = dataHasInputs && (!dataHasOutput || isFilter);
+				// allow an operation if the data has all the inputs and the data doesn't
+				// have the output (i.e. this operation hasn't already been run) *unless*
+				// the operation has the same output as one of the inputs (i.e. it can be
+				// run multiple times on the same data)
+				const shouldAllowOperation = dataHasInputs && (!dataHasOutput || isFilter);
 
-                return {
-                    name: command,
-                    message: desc,
-                    disabled: shouldAllowOperation ? false : '', // empty string to hide "(disabled)" message
-                };
-            }).concat([
-                {role: 'separator'},
-                {name: 'preview'},
-                {name: 'undo', message: 'undo last step', disabled: steps.length > 0 ? false : ''},
-                {name: 'done', hint: `your work is autosaved as ${path.basename(run)}`}
-            ]),
-        });
+				return {
+					name: command,
+					message: desc,
+					disabled: shouldAllowOperation ? false : '', // empty string to hide "(disabled)" message
+				};
+			}).concat([
+				{role: 'separator'},
+				{name: 'preview'},
+				{name: 'undo', message: 'undo last step', disabled: steps.length > 0 ? false : ''},
+				{name: 'done', hint: `your work is autosaved as ${path.basename(run)}`}
+			]),
+		});
 
-        if(thing === 'done') {
-            break;
-        } else if(thing === 'preview') {
-            console.log(data); //eslint-disable-line no-console
-        } else if(thing === 'undo') {
-            const undoneStep = steps.pop(); // remove last operation
-            const undoneOperation = operations[undoneStep.name];
+		if(thing === 'done') {
+			break;
+		} else if(thing === 'preview') {
+			console.log(data); //eslint-disable-line no-console
+		} else if(thing === 'undo') {
+			const undoneStep = steps.pop(); // remove last operation
+			const undoneOperation = operations[undoneStep.name];
 
-            if(undoneOperation.undo) {
-                await undoneOperation.undo(data);
-            }
+			if(undoneOperation.undo) {
+				await undoneOperation.undo(data);
+			}
 
-            delete data[undoneOperation.output]; // clear any output data. if it's a filter step we'll rebuild it
+			delete data[undoneOperation.output]; // clear any output data. if it's a filter step we'll rebuild it
 
-            const stepsToReplay = takeWhileLast(
-                steps,
-                step => operations[step.name].output === undoneOperation.output
-            );
+			const stepsToReplay = takeWhileLast(
+				steps,
+				step => operations[step.name].output === undoneOperation.output
+			);
 
-            steps.splice(steps.length - stepsToReplay.length, stepsToReplay.length);
-            await persist();
+			steps.splice(steps.length - stepsToReplay.length, stepsToReplay.length);
+			await persist();
 
-            // if stepsToReplay is empty this will do nothing
-            await replay(stepsToReplay);
-        } else {
-            await runStep({
-                operation: operations[thing],
-                args: await prompt(operations[thing].args),
-            });
-        }
-    }
+			// if stepsToReplay is empty this will do nothing
+			await replay(stepsToReplay);
+		} else {
+			await runStep({
+				operation: operations[thing],
+				args: await prompt(operations[thing].args),
+			});
+		}
+	}
 };
 
 module.exports = {
-    command: ['*', 'interactive'],
-    desc: 'interactively build steps of a transformation',
-    handler,
+	command: ['*', 'interactive'],
+	desc: 'interactively build steps of a transformation',
+	handler,
 };
--- src/commands/run-script.js
+++ src/commands/run-script.js
@@ -12,8 +12,8 @@ const git = require('@financial-times/git');
 const runProcess = require('../lib/run-process');
 
 exports.args = [
-    {type: 'text', name: 'script', message: 'path to a script'},
-    {type: 'text', name: 'branch', message: 'branch to create'},
+	{type: 'text', name: 'script', message: 'path to a script'},
+	{type: 'text', name: 'branch', message: 'branch to create'},
 ];
 
 const workspacePath = path.join(process.env.HOME, '.config/transformation-runner-workspace');
@@ -27,88 +27,88 @@ const workspacePath = path.join(process.env.HOME, '.config/transformation-runner
  * @param {string} argv.branch
  */
 exports.handler = async ({ script, repos, branch }) => {
-    if(repos.length === 0) {
-        throw new Error('No repos specified');
-    }
+	if(repos.length === 0) {
+		throw new Error('No repos specified');
+	}
 
-    const scriptPath = path.resolve(script);
+	const scriptPath = path.resolve(script);
 
-    if(!(await fs.exists(scriptPath))) {
-        assert(false, `Script does not exist: ${scriptPath}`);
-    }
+	if(!(await fs.exists(scriptPath))) {
+		assert(false, `Script does not exist: ${scriptPath}`);
+	}
 
-    if(!(await fs.exists(scriptPath, fs.constants.X_OK))) {
-        assert(false, `Script is not executable (try \`chmod +x\`): ${scriptPath}`);
-    }
+	if(!(await fs.exists(scriptPath, fs.constants.X_OK))) {
+		assert(false, `Script is not executable (try \`chmod +x\`): ${scriptPath}`);
+	}
 
-    await mkdirp(workspacePath);
+	await mkdirp(workspacePath);
 
-    // TODO konmari logging
-    console.log(`-- Script: ${scriptPath}`);
-    console.log(`-- Target(s):\n\n   ${repos.map(({name}) => name).join('\n   ')}`);
+	// TODO konmari logging
+	console.log(`-- Script: ${scriptPath}`);
+	console.log(`-- Target(s):\n\n   ${repos.map(({name}) => name).join('\n   ')}`);
 
-    const branches = [];
+	const branches = [];
 
-    // TODO parallel
-    for (let repository of repos) {
-        console.log('\n===\n');
+	// TODO parallel
+	for (let repository of repos) {
+		console.log('\n===\n');
 
-        const cloneDirectory = path.join(workspacePath, repository.name);
-        const remoteUrl = `git@github.com:${repository.owner}/${repository.name}.git`;
+		const cloneDirectory = path.join(workspacePath, repository.name);
+		const remoteUrl = `git@github.com:${repository.owner}/${repository.name}.git`;
 
-        git.defaults({ workingDirectory: cloneDirectory });
+		git.defaults({ workingDirectory: cloneDirectory });
 
-        try {
-            if(await fs.exists(cloneDirectory)) {
-                console.log(`-- Updating local clone: ${repository.name}`);
-                await git.checkoutBranch({ name: 'master' });
-                // TODO reset & pull
-                console.log(`-- Repository '${repository.name}' updated in ${cloneDirectory}`);
-            } else {
-                console.log(`-- Cloning repository locally: ${remoteUrl}`);
-                await git.clone({ origin: 'origin', repository: remoteUrl });
-                console.log(`-- Repository '${repository.name}' cloned locally to ${cloneDirectory}`);
-            }
+		try {
+			if(await fs.exists(cloneDirectory)) {
+				console.log(`-- Updating local clone: ${repository.name}`);
+				await git.checkoutBranch({ name: 'master' });
+				// TODO reset & pull
+				console.log(`-- Repository '${repository.name}' updated in ${cloneDirectory}`);
+			} else {
+				console.log(`-- Cloning repository locally: ${remoteUrl}`);
+				await git.clone({ origin: 'origin', repository: remoteUrl });
+				console.log(`-- Repository '${repository.name}' cloned locally to ${cloneDirectory}`);
+			}
 
-            await git.createBranch({ name: branch });
-            await git.checkoutBranch({ name: branch });
-            console.log(`-- Created and checked out new branch in local repository: ${branch}`);
+			await git.createBranch({ name: branch });
+			await git.checkoutBranch({ name: branch });
+			console.log(`-- Created and checked out new branch in local repository: ${branch}`);
 
-            const contextForScript = {
-                TRANSFORMATION_RUNNER_RUNNING: true,
-                TRANSFORMATION_RUNNER_TARGET: remoteUrl,
-                TRANSFORMATION_RUNNER_TARGET_NAME: repository.name,
-            };
+			const contextForScript = {
+				TRANSFORMATION_RUNNER_RUNNING: true,
+				TRANSFORMATION_RUNNER_TARGET: remoteUrl,
+				TRANSFORMATION_RUNNER_TARGET_NAME: repository.name,
+			};
 
-            const scriptEnv = {
-                ...process.env,
-                ...contextForScript
-            };
+			const scriptEnv = {
+				...process.env,
+				...contextForScript
+			};
 
-            console.log(`-- Running script against local repository...\n`);
+			console.log(`-- Running script against local repository...\n`);
 
-            const scriptOutput  = await runProcess(
-                scriptPath,
-                {
-                    cwd: cloneDirectory,
-                    env: scriptEnv
-                }
-            );
+			const scriptOutput  = await runProcess(
+				scriptPath,
+				{
+					cwd: cloneDirectory,
+					env: scriptEnv
+				}
+			);
 
-            console.log(scriptOutput);
+			console.log(scriptOutput);
 
-            // TODO don't push if no changes
-            console.log(`-- Pushing branch ${branch} to remote 'origin'`);
-            await git.push({ repository: 'origin', refspec: branch });
+			// TODO don't push if no changes
+			console.log(`-- Pushing branch ${branch} to remote 'origin'`);
+			await git.push({ repository: 'origin', refspec: branch });
 
-            branches.push(branch);
-        } catch (error) {
-            console.error(new Error(`Error running script for '${repository.name}': ${error.message}`));
-            throw error;
-        }
-    }
+			branches.push(branch);
+		} catch (error) {
+			console.error(new Error(`Error running script for '${repository.name}': ${error.message}`));
+			throw error;
+		}
+	}
 
-    return branches;
+	return branches;
 };
 
 exports.command = 'run-script',
