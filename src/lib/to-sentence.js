4e7165bdf313c0bdd79cafb987130ab31105e3a5
bren <bren@153.io>
2019-04-02 15:56:06 +0100

    who gives a fuck about an oxford comma
    - add oxford commas to toSentence
    - also document toSentence
    - https://www.youtube.com/watch?v=P_i1xk07o4g

--- src/lib/to-sentence.js
+++ src/lib/to-sentence.js
@@ -1,5 +1,11 @@
+// formats an array of strings ['foo', 'bar', 'baz'] as a
+// comma-and-and separated string 'foo, bar, and baz'
 const toSentence = words => {
 	let string = words.slice(0, -1).join(', ');
+
+	if(words.length > 2) {
+		string += ', ';
+	}
 
 	if(words.length > 1) {
 		string += ' and ';
