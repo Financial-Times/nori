284f0fa07b23aad0609f1a36e9d17ad4d47442ae
bren <bren@153.io>
2019-06-17 14:13:43 +0100

    tests for get-project

--- __mocks__/@octokit/rest.js
+++ __mocks__/@octokit/rest.js
@@ -1,36 +1,43 @@
 module.exports = function Octokit() {
 	return {
+		paginate: jest.fn().mockImplementation(async args => {
+			if (args === 'mock listForOrg.endpoint.merge data') {
+				return [{ number: 137, id: 'mock project id' }]
+			}
+		}),
 		projects: {
-			createForOrg: jest.fn().mockReturnValue(
-				Promise.resolve({
-					data: {
-						id: 'mock project id',
-					},
-				}),
-			),
-			createColumn: jest.fn().mockReturnValue(
-				Promise.resolve({
-					data: {
-						id: 'mock column id',
-					},
-				}),
-			),
-			createCard: jest.fn().mockReturnValue(
-				Promise.resolve({
-					data: {
-						id: 'mock card id',
-					},
-				}),
-			),
+			createForOrg: jest.fn().mockResolvedValue({
+				data: {
+					id: 'mock project id',
+				},
+			}),
+			createColumn: jest.fn().mockResolvedValue({
+				data: {
+					id: 'mock column id',
+				},
+			}),
+			createCard: jest.fn().mockResolvedValue({
+				data: {
+					id: 'mock card id',
+				},
+			}),
 			deleteCard: jest.fn(),
 			update: jest.fn(),
+			listForOrg: Object.assign(jest.fn(), {
+				endpoint: {
+					merge: jest
+						.fn()
+						.mockReturnValue('mock listForOrg.endpoint.merge data'),
+				},
+			}),
+			listColumns: jest
+				.fn()
+				.mockResolvedValue({ data: [{ id: 'mock column id' }] }),
 		},
 		pulls: {
-			create: jest.fn().mockReturnValue(
-				Promise.resolve({
-					data: { id: 'mock pr id' },
-				}),
-			),
+			create: jest.fn().mockResolvedValue({
+				data: { id: 'mock pr id' },
+			}),
 			update: jest.fn(),
 		},
 		issues: {
---
+++ test/commands/get-project.test.js
@@ -0,0 +1,50 @@
+const project = require('../../src/commands/get-project')
+const getOctokit = require('../../src/lib/octokit')
+
+const githubAccessToken = 'mock access token'
+const octokit = getOctokit(githubAccessToken)
+
+test('correctly throws an error if given incorrect arguments', () => {
+	const projectUrlArg = project.args.find(arg => arg.name === 'projectUrl')
+
+	expect(projectUrlArg.validate('not a github url')).toBe(
+		'Please enter a valid GitHub organisation project URL',
+	)
+	expect(
+		projectUrlArg.validate('https://github.com/orgs/org/projects/137'),
+	).toEqual(true)
+})
+
+test('adds project board by number to state', async () => {
+	const projectUrl = 'https://github.com/orgs/org/projects/137'
+
+	const state = {}
+	await project.handler(
+		{
+			projectUrl,
+			githubAccessToken,
+		},
+		state,
+	)
+
+	expect(octokit.projects.listForOrg.endpoint.merge).toHaveBeenCalledWith({
+		org: 'org',
+	})
+
+	expect(state).toEqual({
+		project: {
+			id: 'mock project id',
+			number: 137,
+			columns: [{ id: 'mock column id' }],
+		},
+	})
+})
+
+test('undo removes data from state', async () => {
+	const state = {
+		project: { id: 'mock project id' },
+	}
+
+	await project.undo({ githubAccessToken }, state)
+	expect(state).toEqual({})
+})
