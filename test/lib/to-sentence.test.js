1d96f8e907dbe590e864eef7127936872dcb9ab5
bren <bren@153.io>
2019-06-20 12:19:06 +0100

    remove foo, bar etc from tests

--- test/lib/to-sentence.test.js
+++ test/lib/to-sentence.test.js
@@ -1,25 +1,15 @@
-const toSentence = require('../../src/lib/to-sentence');
+const toSentence = require('../../src/lib/to-sentence')
 
 test('should return only string untouched', () => {
-	expect(
-		toSentence(['foo'])
-	).toEqual(
-		'foo'
-	);
-});
+	expect(toSentence(['quick'])).toEqual('quick')
+})
 
 test('should join two strings with and', () => {
-	expect(
-		toSentence(['foo', 'bar'])
-	).toEqual(
-		'foo and bar'
-	);
-});
+	expect(toSentence(['quick', 'brown'])).toEqual('quick and brown')
+})
 
 test('should join more than two strings with commas and and', () => {
-	expect(
-		toSentence(['foo', 'bar', 'baz', 'quux'])
-	).toEqual(
-		'foo, bar, baz, and quux'
-	);
-});
+	expect(toSentence(['quick', 'brown', 'lazy', 'fox'])).toEqual(
+		'quick, brown, lazy, and fox',
+	)
+})
