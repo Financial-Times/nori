78d1fe78b466dbe855a215c81a9c126cf3220013
bren <bren@153.io>
2019-07-01 13:28:14 +0100

    fix error state in promiseAllErrors (don't shadow error)

--- src/lib/promise-all-errors.js
+++ src/lib/promise-all-errors.js
@@ -1,22 +1,26 @@
 // like Promise.all, but waits for every promise to complete even if one errors
 
-const error = Symbol('error')
+const errorSymbol = Symbol('error')
 
 module.exports = promises =>
 	Promise.all(
 		// Promise.all can accept an iterable, but we need to map it,
 		// so use Array.from instead of assuming Array and using .map
 		Array.from(promises, promise =>
-			promise.then(result => result, error => ({ [error]: error })),
+			promise.catch(error => ({ [errorSymbol]: error })),
 		),
 	).then(results => {
-		const anyError = results.find(result => Boolean(result && result[error]))
+		const anyError = results.find(result =>
+			Boolean(result && result[errorSymbol]),
+		)
 
 		if (anyError) {
 			// throw the first error, and attach the errors and successes to it as array properties
-			anyError.successes = results.filter(result => !(result && result[error]))
+			anyError.successes = results.filter(
+				result => !(result && result[errorSymbol]),
+			)
 			anyError.failures = results
-				.map(result => result && result[error])
+				.map(result => result && result[errorSymbol])
 				.filter(Boolean)
 			throw anyError
 		}
